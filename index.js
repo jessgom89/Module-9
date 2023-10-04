e99feab228c9f325ae913a4e58f2c28543f606e7
jessgom89 <gomezjessica933@yahoo.com>
2023-10-03 19:58:17 -0700

    22

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    // THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions  
+    {
+     type: 'input',
+     name: 'title',
+     message:'What is your title?',   
+    }
+    
+    
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
